4d7e9eed6d1851803e5d1f624322d1739762ef11
mouwade <ahmedwade001@gmail.com>
2025-10-16 22:49:37 +0000

    add lab Build an All-True Property Validator

---
+++ lab/Higher Order Functions and Callbacks/Build an All-True Property Validator.js
@@ -0,0 +1,7 @@
+function truthCheck(collection, pre) {
+  return !collection.some((obj)=>!obj[pre])
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"));
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"))
